1c211ce02fc6eab45b11971455dba287c14fb55c
Yang Jiaye <yjy420@sjtu.edu.cn>
2024-04-16 15:59:00 +0800

    update

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ from tqdm import tqdm
 
 import test as test    # import test.py to get mAP after each epoch
 from models.experimental import attempt_load
-from models.yolo import Model
+from models.yolo_Ncla import Model
 from utils.autoanchor import check_anchors
 from utils.datasets import create_dataloader
 from utils.general import labels_to_class_weights, increment_path, labels_to_image_weights, init_seeds, \
